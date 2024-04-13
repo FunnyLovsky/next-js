7a147c2455b19d0a1d688b551d392a2238d67519
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-04-13 15:48:45 +0300

    start learn

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return(
+        <h1>Aboud PAge</h1>
+    )
+}
+
+export default About
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Index = () => {
+    return(
+        <>
+                <h1>Hellddo Next!</h1>
+                <style jsx>
+                    {`
+                        h1 {
+                            color: red;
+                        }
+                    `}
+                </style>
+        </>
+
+    )
+}
+
+export default Index;
