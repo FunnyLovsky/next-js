1eb9215fd0d959c0c19bf03ee4d5f11937224a09
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-04-23 17:43:32 +0300

    fix: seo

--- src/widgets/MetaLayout/ui/MetaLayout.tsx
+++ src/widgets/MetaLayout/ui/MetaLayout.tsx
@@ -1,40 +1,38 @@
 import { IPropsChildren } from '@/shared/types/IComponents'
 import Head from 'next/head'
 import { FC } from 'react'
+import { metaData } from '../const/metaData'
 
 interface IProps extends IPropsChildren {
     title: string
+    description?: string
+    keywords?: string
+    ogDescription?: string
 }
 
-const MetaLayout: FC<IProps> = ({ title, children }) => {
+const MetaLayout: FC<IProps> = ({
+    title,
+    children,
+    description = metaData.description,
+    keywords = metaData.keywords,
+    ogDescription = metaData.ogDescription,
+}) => {
     return (
         <>
             <Head>
-                <meta
-                    name="description"
-                    content="GLAMIFY - интернет-магазин одежды для стильных и модных. Широкий выбор женской, мужской и детской одежды высокого качества."
-                />
-                <meta
-                    name="keywords"
-                    content="GLAMIFY, одежда, интернет-магазин, мода, стиль, мужская одежда, женская одежда"
-                />
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
                 <meta name="author" content="GLAMIFY" />
                 <meta property="og:title" content={title} />
-                <meta
-                    property="og:description"
-                    content="Широкий выбор стильной и модной одежды для женщин, мужчин и детей. Быстрая доставка и высокое качество."
-                />
+                <meta property="og:description" content={ogDescription} />
                 <meta property="og:image" content="https://glamify-shop.vercel.app/favicon.svg" />
                 <meta property="og:url" content="https://glamify-shop.vercel.app" />
                 <meta property="og:type" content="website" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content={title} />
-                <meta
-                    name="twitter:description"
-                    content="Широкий выбор стильной и модной одежды для женщин, мужчин и детей. Быстрая доставка и высокое качество."
-                />
+                <meta name="twitter:description" content={ogDescription} />
                 <meta name="twitter:image" content="https://glamify-shop.vercel.app/favicon.svg" />
-                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
                 <title>{title}</title>
             </Head>
             {children}
