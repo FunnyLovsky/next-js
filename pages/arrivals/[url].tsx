ba673d2c037819ee5ae2b8b44b7a8e4eb6de17d6
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-04-24 21:48:03 +0300

    dev: dynamic url

--- pages/arrivals/[url].tsx
+++ pages/arrivals/[url].tsx
@@ -1,16 +1,30 @@
-import { NavLayout } from '@/widgets/NavLayout'
-import { Head } from 'next/document'
-import { useRouter } from 'next/router'
+import { IProductDetail, Services } from '@/entities/Product'
+import { ProductDetailPage } from '@/pages/ProductDetail'
+import { MetaLayout } from '@/widgets/MetaLayout'
+import { FC } from 'react'
 
-export default function ShopID() {
-    const { query } = useRouter()
+export const getServerSideProps = async (context) => {
+    const { url } = context.params!
+    try {
+        const response = await Services.getDetailProduct(url)
+        return { props: { product: response.data, error: null } }
+    } catch (error) {
+        return { props: { product: null, error: error.message } }
+    }
+}
 
+interface IProps {
+    product: IProductDetail
+    error: null | string
+}
+
+const ShopID: FC<IProps> = ({ product, error }) => {
+    const title = product ? `${product.name} - GLAMIFY` : 'Товар не найден - GLAMIFY'
     return (
-        <>
-            <title>Glamify - {query.url}</title>
-
-            <h2>Shop URL</h2>
-            <p>{query.url}</p>
-        </>
+        <MetaLayout title={title}>
+            <ProductDetailPage productDetail={product} error={error} />
+        </MetaLayout>
     )
 }
+
+export default ShopID
--- pages/shop/[url].tsx
+++ pages/shop/[url].tsx
@@ -1,15 +1,30 @@
+import { IProductDetail, Services } from '@/entities/Product'
+import { ProductDetailPage } from '@/pages/ProductDetail'
 import { MetaLayout } from '@/widgets/MetaLayout'
-import { NavLayout } from '@/widgets/NavLayout'
-import { Head } from 'next/document'
-import { useRouter } from 'next/router'
+import { FC } from 'react'
 
-export default function ShopID() {
-    const { query } = useRouter()
+export const getServerSideProps = async (context) => {
+    const { url } = context.params!
+    try {
+        const response = await Services.getDetailProduct(url)
+        return { props: { product: response.data, error: null } }
+    } catch (error) {
+        return { props: { product: null, error: error.message } }
+    }
+}
 
+interface IProps {
+    product: IProductDetail
+    error: null | string
+}
+
+const ShopID: FC<IProps> = ({ product, error }) => {
+    const title = product ? `${product.name} - GLAMIFY` : 'Товар не найден - GLAMIFY'
     return (
-        <MetaLayout title={`Glamify - ${query.url}`}>
-            <h2>Shop URL</h2>
-            <p>{query.url}</p>
+        <MetaLayout title={title}>
+            <ProductDetailPage productDetail={product} error={error} />
         </MetaLayout>
     )
 }
+
+export default ShopID
