8bd3fe0c0a77b8b08242cdf8f5724b0541e6bcc1
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-04-25 16:32:12 +0300

    fix: cache

--- src/entities/Product/api/service.ts
+++ src/entities/Product/api/service.ts
@@ -4,6 +4,7 @@ import { errorHandler } from '@/shared/lib/errorHandler'
 import { IProductDetail } from '../types/IProduct'
 import { ProductsResponse } from '../model/types/ProductsResponse'
 import { API_URL } from '@/shared/const/URL'
+import { get } from 'http'
 
 export class Services {
     static async getDetailProduct(url: string): Promise<AxiosResponse<IProductDetail>> {
@@ -23,45 +24,23 @@ export class Services {
     }
 
     static async getProductsCache(query: string) {
-        const url = `${API_URL}/products?${query}`
-        const cache = await caches.open('getProducts')
-        console.log('open cache');
-        
-
-        const cachedResponse = await cache.match(url)
-
-        if(cachedResponse) {
-            const products = await cachedResponse.json();
-            console.log('get cache');
-            
-            return {data: {products: products.products}}
-        }
-
-        const response = await fetch(url)
-        await cache.put(url, response)
-        console.log(cache.keys());
-        
-        console.log('step 1');
-
-        if(response.ok) {
-            const products = await response.json()
-            console.log('step 2');
-            console.log(products);
-            
-            return {data: {
-                products: products.products
-            }}
-        } else {
-            console.log('error');
-            
-            throw new Error('Хер знает что не так')
-        }
         try {
+            const url = `${API_URL}/products?${query}`
+            const cache = await caches.open('getProducts')
 
+            const cachedResponse = await cache.match(url)
+    
+            if(cachedResponse) {
+                return await cachedResponse.json();
+            }
+    
+            const response = await this.getProducts(query)
+            await cache.put(url, new Response(JSON.stringify(response)))
+ 
+            return response
             
-
         } catch (error) {
-            // return errorHandler(error)
+            return errorHandler(error)
         }
     }
 }
--- src/entities/Product/model/services/useFetchProducts.tsx
+++ src/entities/Product/model/services/useFetchProducts.tsx
@@ -13,10 +13,8 @@ export const useFetchProducts = (query: string, limit: number = 4, page: number 
         try {
             setIsLoading(true)
             const product = await Services.getProductsCache(`limit=${limit}&page=${page}&${query}`)
-            console.log(product)
-
-            // setProducts(product.data.products)
-            // setIsLoading(false)
+            setProducts(product.data.products)
+            setIsLoading(false)
         } catch (error) {
             setProducts([])
             setIsLoading(false)
