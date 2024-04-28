2b4d108fdab05e13049885cbb61390f2f74522c6
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-04-28 17:10:00 +0300

    verif: yandex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ export default function Document() {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
+                <meta name="yandex-verification" content="9e207b9544950d43" />
             </Head>
             <body>
                 <div id="modal" />
